93ff4880fa92ca67b8c226c8f36bdb7fb0cfb676
DoniaAli77 <donia.ali.7799@gmail.com>
2024-03-19 22:53:46 +0200

    fixed stateful part

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 
-// ------------------------------(StateFull Version)----------------------------------
+// ------------------------------(StateFul Version)----------------------------------
 
 class myHome extends StatefulWidget {
   @override
