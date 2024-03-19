722efea585b0163241241f638c5ec4c83eee4f12
DoniaAli77 <donia.ali.7799@gmail.com>
2024-03-19 22:49:17 +0200

    fixed stateful part

--- lib/main.dart
+++ lib/main.dart
@@ -18,116 +18,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo', 
         theme: ThemeData(),
-         home: myHomeHook() // aw myHome1()
+         home: myHome() // aw myHomeHook()
          );
   }
 }
 
 
+// ------------------------------(StateFull Version)----------------------------------
 
-class myHome1 extends StatefulWidget{
+class myHome extends StatefulWidget {
   @override
-  State<myHome1> createState() => _myHome1State();
+  State<myHome> createState() => _myHomeeState();
 }
-class _myHome1State extends State<myHome1> {
-  var ql = ['what is your fav color', 'what is your least fav color'];
-  var indx = 0;
-  void doSomething() {
+
+class _myHomeeState extends State<myHome> {
+  var questionIndex = 0;
+
+  var questionList = [
+    {
+      'q': 'Favorite color?',
+      'a': [
+        {'cname': 'blue', 'score': 5},
+        {'cname': 'yellow', 'score': 4},
+        {'cname': 'green', 'score': 3}
+      ]
+    },
+    {
+      'q': 'Least favorite color?',
+      'a': [
+        {'cname': 'black', 'score': -5},
+        {'cname': 'magenta', 'score': -4},
+        {'cname': 'purple', 'score': -3},
+        {'cname': 'red', 'score': -2}
+      ]
+    }
+  ];
+  var totalScore = 0;
+  void doSomething(int score) {
+    totalScore += score;
     setState(() {
-         indx++;
-    print(indx);
+      questionIndex++;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Giu First app'),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Column(
-        children: [
-          Question(ql[indx]),
-          ElevatedButton(onPressed: doSomething, child: Text('blue')),
-          ElevatedButton(onPressed: doSomething, child: Text('green')),
-          ElevatedButton(onPressed: doSomething, child: Text('red'))
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Personality Test'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: questionIndex < questionList.length
+            ? QuizPart(
+                qList: questionList,
+                qIndex: questionIndex,
+                cbFunction: doSomething,
+              )
+            : Center(
+                child: Text(
+                  totalScore < 0 ? "Introvert" : "Extrovert",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
       ),
     );
   }
 }
-
-// class myHome extends StatefulWidget {
-//   @override
-//   State<myHome> createState() => _myHomeeState();
-// }
-// class _myHomeeState extends State<myHome> {
-//   var ql = ['what is your fav color', 'what is your least fav color'];
-//   var indx = 0;
-//   void doSomething() {
-//     setState(() {
-//       indx++;
-//     });
-//     print(indx);
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Giu first App'),
-//         backgroundColor: Colors.amber,
-//       ),
-//       body: Center(
-//         child: Column(children: <Widget>[
-//           Question(ql[indx]),
-//           MyButton(buttonText: "Blue", buttonCallback: doSomething),
-//           MyButton(buttonText: "Green", buttonCallback: doSomething),
-//           MyButton(buttonText: "Red", buttonCallback: doSomething)
-//         ]),
-//       ),
-//     );
-//   }
-// }
-
-  //  {'cname': 'black', 'score': -5},
-//         {'cname': 'magenta', 'score': -4},
-//         {'cname': 'purple', 'score': -3},
-//         {'cname': 'red', 'score': -2}
-//       ]
-//     }
-//   ];
-//   var totalScore = 0;
-//   void doSomething(int score) {
-//     totalScore += score;
-//     setState(() {
-//       questionIndex++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text("GUC first app"),
-//           backgroundColor: Colors.blueAccent,
-//         ),
-//         body: questionIndex < questionList.length
-//             ? QuizPart(
-//                 qList: questionList,
-//                 qIndex: questionIndex,
-//                 cbFunction: doSomething,
-//               )
-//             : Center(
-//                 child: Text(
-//                   totalScore < 0 ? "Introvert" : "Extrovert",
-//                   style: TextStyle(fontSize: 30),
-//                 ),
-//               ),
-//       ),
-//     );
-//   }
-// }
 
 // ------------------------------(Hoooooooook Version)----------------------------------
 class myHomeHook extends HookWidget {
@@ -162,7 +119,7 @@ class myHomeHook extends HookWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("GUC first app"),
+          title: Text("Personality Test"),
           backgroundColor: Colors.blueAccent,
         ),
         body: questionIndex.value < questionList.length
