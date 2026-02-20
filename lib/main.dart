b29575b8c5da398c0d6806744083e38318f811a8
DoniaAli77 <donia.ali.7799@gmail.com>
2026-02-21 01:56:07 +0200

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+// import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'myButton.dart';
 import 'question.dart';
@@ -87,55 +87,55 @@ class _myHomeeState extends State<myHome> {
 }
 
 // ------------------------------(Hoooooooook Version)----------------------------------
-class myHomeHook extends HookWidget {
-  @override
-  Widget build(BuildContext context) {
-    final questionIndex = useState(0);
+// class myHomeHook extends HookWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final questionIndex = useState(0);
 
-    var questionList = [
-      {
-        'q': 'Favorite color?',
-        'a': [
-          {'cname': 'blue', 'score': 5},
-          {'cname': 'yellow', 'score': 4},
-          {'cname': 'green', 'score': 3}
-        ]
-      },
-      {
-        'q': 'Least favorite color?',
-        'a': [
-          {'cname': 'black', 'score': -5},
-          {'cname': 'magenta', 'score': -4},
-          {'cname': 'purple', 'score': -3},
-          {'cname': 'red', 'score': -2}
-        ]
-      }
-    ];
-    var totalScore = 0;
-    void doSomething(int score) {
-      totalScore += score;
-      questionIndex.value++;
-    }
+//     var questionList = [
+//       {
+//         'q': 'Favorite color?',
+//         'a': [
+//           {'cname': 'blue', 'score': 5},
+//           {'cname': 'yellow', 'score': 4},
+//           {'cname': 'green', 'score': 3}
+//         ]
+//       },
+//       {
+//         'q': 'Least favorite color?',
+//         'a': [
+//           {'cname': 'black', 'score': -5},
+//           {'cname': 'magenta', 'score': -4},
+//           {'cname': 'purple', 'score': -3},
+//           {'cname': 'red', 'score': -2}
+//         ]
+//       }
+//     ];
+//     var totalScore = 0;
+//     void doSomething(int score) {
+//       totalScore += score;
+//       questionIndex.value++;
+//     }
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Personality Test"),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: questionIndex.value < questionList.length
-            ? QuizPart(
-                qList: questionList,
-                qIndex: questionIndex.value,
-                cbFunction: doSomething,
-              )
-            : Center(
-                child: Text(
-                  totalScore < 0 ? "Introvert" : "Extrovert",
-                  style: TextStyle(fontSize: 30),
-                ),
-              ),
-      ),
-    );
-  }
-}
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Personality Test"),
+//           backgroundColor: Colors.blueAccent,
+//         ),
+//         body: questionIndex.value < questionList.length
+//             ? QuizPart(
+//                 qList: questionList,
+//                 qIndex: questionIndex.value,
+//                 cbFunction: doSomething,
+//               )
+//             : Center(
+//                 child: Text(
+//                   totalScore < 0 ? "Introvert" : "Extrovert",
+//                   style: TextStyle(fontSize: 30),
+//                 ),
+//               ),
+//       ),
+//     );
+//   }
+// }
